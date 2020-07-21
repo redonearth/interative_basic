03897d5d055319e8a13f5aa4cbab7b57dee71635
Redonearth <wona23@gmail.com>
2020-07-21 21:55:54 +0900

    JS 클릭 이벤트 코드 추가

---
+++ js/three_doors.js
@@ -0,0 +1,20 @@
+(() => {
+  const stageElem = document.querySelector(".stage");
+  // 현재 활성화된 아이템을 저장
+  let currentItem;
+
+  function doorHandler(e) {
+    const targetElem = e.target;
+
+    if (currentItem) {
+      currentItem.classList.remove("door-opened");
+    }
+
+    if (targetElem.classList.contains("door-body")) {
+      targetElem.parentNode.classList.add("door-opened");
+      currentItem = targetElem.parentNode;
+    }
+  }
+
+  stageElem.addEventListener("click", doorHandler);
+})();
