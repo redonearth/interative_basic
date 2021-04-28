c9b119b402ed8441b09c844165246bd88d5cd6cd
Jisang Hong <wona23@gmail.com>
2021-04-28 14:36:53 +0900

    초기 화면 애니메이션 추가

--- interactive-intermediate/js/main.js
+++ interactive-intermediate/js/main.js
@@ -103,6 +103,10 @@
     }
   });
 
+  leaflet.addEventListener("animationend", () => {
+    leaflet.style.animation = "none";
+  });
+
   window.addEventListener("mousemove", (e) => {
     targetPos.x = e.clientX - window.innerWidth * 0.7;
     targetPos.y = e.clientY - window.innerHeight * 0.7;
