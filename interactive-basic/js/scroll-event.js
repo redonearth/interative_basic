cea51ffa4021c988f2f9be44145ef235c0812ee5
Jisang Hong <wona23@gmail.com>
2021-04-28 16:57:51 +0900

    스크롤 위치에 따라 오브젝트 조작하기 학습

--- interactive-basic/js/scroll-event.js
+++ interactive-basic/js/scroll-event.js
@@ -1,6 +1,22 @@
 (() => {
   const outputElem = document.querySelector(".output");
+  const ilbuniElem = document.querySelector(".ilbuni");
+
+  function showValue() {
+    // outputElem.innerHTML = window.pageYOffset;
+    // outputElem.innerHTML = ilbuniElem.offsetTop;
+    let posY = ilbuniElem.getBoundingClientRect().top;
+    outputElem.innerHTML = posY;
+
+    if (posY < window.innerHeight / 5) {
+      ilbuniElem.classList.add("zoom");
+    } else {
+      ilbuniElem.classList.remove("zoom");
+    }
+  }
+
   window.addEventListener("scroll", () => {
-    outputElem.innerHTML = window.pageYOffset;
+    showValue();
   });
+  showValue();
 })();
