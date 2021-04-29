f45d8a50b8d2ad266bf4da91aba0c42ed5f50d23
Jisang Hong <wona23@gmail.com>
2021-04-29 14:44:41 +0900

    Window resize 이벤트 추가

--- interactive-basic/3d-wall/js/wall3d.js
+++ interactive-basic/3d-wall/js/wall3d.js
@@ -1,9 +1,16 @@
 (() => {
   const houseElem = document.querySelector(".house");
-  let maxScrollValue = document.body.offsetHeight - window.innerHeight;
+  let maxScrollValue;
+
+  function resizeHandler() {
+    maxScrollValue = document.body.offsetHeight - window.innerHeight;
+  }
 
   window.addEventListener("scroll", () => {
     const zMove = (pageYOffset / maxScrollValue) * 980 - 490;
     houseElem.style.transform = `translateZ(${zMove}vw)`;
   });
+
+  window.addEventListener("resize", resizeHandler);
+  resizeHandler();
 })();
