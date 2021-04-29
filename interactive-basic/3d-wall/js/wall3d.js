d1dcf7a93fbfa57eb296b47357395d599c631cad
Jisang Hong <wona23@gmail.com>
2021-04-29 14:36:38 +0900

    스크롤 시 Z축 이동 구현

--- interactive-basic/3d-wall/js/wall3d.js
+++ interactive-basic/3d-wall/js/wall3d.js
@@ -1,1 +1,9 @@
-(() => {})();
+(() => {
+  const houseElem = document.querySelector(".house");
+  let maxScrollValue = document.body.offsetHeight - window.innerHeight;
+
+  window.addEventListener("scroll", () => {
+    const zMove = (pageYOffset / maxScrollValue) * 980 - 490;
+    houseElem.style.transform = `translateZ(${zMove}vw)`;
+  });
+})();
