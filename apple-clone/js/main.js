142314711fc924774bbd3981b081638347fd041d
Jisang Hong <wona23@gmail.com>
2021-05-11 15:07:18 +0900

    현재 활성 Scene 반영하는 코드 추가

--- apple-clone/js/main.js
+++ apple-clone/js/main.js
@@ -49,6 +49,17 @@
         ${sceneInfo[i].scrollHeight}px
       `;
     }
+
+    yOffset = window.pageYOffset;
+    let totalScrollHeight = 0;
+    for (let i = 0; i < sceneInfo.length; i++) {
+      totalScrollHeight += sceneInfo[i].scrollHeight;
+      if (totalScrollHeight >= yOffset) {
+        currentScene = i;
+        break;
+      }
+    }
+    document.body.setAttribute('id', `show-scene-${currentScene}`);
   };
 
   const scrollLoop = () => {
@@ -58,19 +69,19 @@
     }
     if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
       currentScene++;
+      document.body.setAttribute('id', `show-scene-${currentScene}`);
     }
     if (yOffset < prevScrollHeight) {
       if (currentScene === 0) return;
       currentScene--;
+      document.body.setAttribute('id', `show-scene-${currentScene}`);
     }
-    console.log(currentScene);
   };
 
-  window.addEventListener('resize', setLayout);
   window.addEventListener('scroll', () => {
     yOffset = window.pageYOffset;
     scrollLoop();
   });
-
-  setLayout();
+  window.addEventListener('load', setLayout);
+  window.addEventListener('resize', setLayout);
 })();
