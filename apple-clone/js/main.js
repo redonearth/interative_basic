21a3a7fb8a4cf9ed8a16d61b893703c59203d62c
Jisang Hong <wona23@gmail.com>
2021-05-11 13:55:18 +0900

    활성 Scene 인식 테스트

--- apple-clone/js/main.js
+++ apple-clone/js/main.js
@@ -1,4 +1,8 @@
 (() => {
+  let yOffset = 0; // window.pageYOffset 대신 사용할 변수
+  let prevScrollHeight = 0; // 현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값의 합
+  let currentScene = 0; // 현재 활성화된(눈 앞에 보고 있는) Scene(scroll-section)
+
   const sceneInfo = [
     {
       // 0
@@ -47,7 +51,26 @@
     }
   };
 
+  const scrollLoop = () => {
+    prevScrollHeight = 0;
+    for (let i = 0; i < currentScene; i++) {
+      prevScrollHeight += sceneInfo[i].scrollHeight;
+    }
+    if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+      currentScene++;
+    }
+    if (yOffset < prevScrollHeight) {
+      if (currentScene === 0) return;
+      currentScene--;
+    }
+    console.log(currentScene);
+  };
+
   window.addEventListener('resize', setLayout);
+  window.addEventListener('scroll', () => {
+    yOffset = window.pageYOffset;
+    scrollLoop();
+  });
 
   setLayout();
 })();
